8919671c0cc0a0379c5cc2b8ad66d88832b4a1d0
koo21 <sawaddiju@hotmail.com>
2023-10-28 13:31:23 +0700

    first commit

---
+++ mylaravel-101/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eveniet et exercitationem, dicta ipsum, assumenda
+        velit ad eos fuga asperiores rem repellendus ducimus optio consectetur maiores architecto minus possimus ab.</p>
+</body>
+
+</html>
