7a58dfce2cbf20a2b955a65b7a7fcfb611d5fc14
koo21 <sawaddiju@hotmail.com>
2023-10-28 14:47:44 +0700

    add 1

---
+++ mylaravel-101/resources/views/layout.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto ">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="col-md-12">
+            @yield('content')
+        </div>
+    </div>
+
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- mylaravel-101/resources/views/welcome.blade.php
+++ mylaravel-101/resources/views/welcome.blade.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Piyawat Sawaddiju</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente non ipsum neque odio. Mollitia est possimus
-        accusamus consequatur hic, aspernatur in sunt consequuntur laboriosam voluptates et quam ullam eaque dolorem.
-    </p>
-</body>
-
-</html>
+@extends('layout')
+@section('title')
+    MyHome
+@endsection
+@section('content')
+<h1>Piyawat Sawaddiju</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente non ipsum neque odio. Mollitia est possimus
+    accusamus consequatur hic, aspernatur in sunt consequuntur laboriosam voluptates et quam ullam eaque dolorem.
+</p>
+@endsection
