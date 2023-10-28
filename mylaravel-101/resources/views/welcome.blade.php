59a840a19893a722ef1d4b174b1e4f72487e6261
koo21 <sawaddiju@hotmail.com>
2023-10-28 13:40:44 +0700

    up

--- mylaravel-101/resources/views/welcome.blade.php
+++ mylaravel-101/resources/views/welcome.blade.php
@@ -9,6 +9,9 @@
 
 <body>
     <h1>Piyawat Sawaddiju</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente non ipsum neque odio. Mollitia est possimus
+        accusamus consequatur hic, aspernatur in sunt consequuntur laboriosam voluptates et quam ullam eaque dolorem.
+    </p>
 </body>
 
 </html>
