9d4c88573c13e2bd876bae1939341d348cad0515
koo21 <sawaddiju@hotmail.com>
2023-10-30 16:35:52 +0700

    เพิ่มข้อมูลใหม่

--- mylaravel-101/resources/views/about.blade.php
+++ mylaravel-101/resources/views/about.blade.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eveniet et exercitationem, dicta ipsum, assumenda
-        velit ad eos fuga asperiores rem repellendus ducimus optio consectetur maiores architecto minus possimus ab.</p>
-</body>
-
-</html>
+@extends('layout')
+@section('title')
+   about
+@endsection
+@section('content')
+<h1>about</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente non ipsum neque odio. Mollitia est possimus
+    accusamus consequatur hic, aspernatur in sunt consequuntur laboriosam voluptates et quam ullam eaque dolorem.
+</p>
+@endsection
